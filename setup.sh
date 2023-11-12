bc33f30b52067f71de370c67c0b4cb77765b1688
saturncloudapp1 <149740534+saturncloudapp1@users.noreply.github.com>
2023-11-11 21:00:26 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,13 +2,4 @@
 # Instala as dependências do Node.js usando npm
 npm install --prefix /home/jovyan/work
 
-# Verifica se a instalação foi bem-sucedida
-if [ $? -eq 0 ]; then
-    echo "Dependências instaladas com sucesso."
-
-    # Executa o arquivo index.js
-    node /home/jovyan/work/index.js
-else
-   node /home/jovyan/work/index.js
-    exit 1
-fi
+node /home/jovyan/work/index.js
