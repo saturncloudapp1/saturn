9e0688d805785a41226f61d121b7122ff8de8a7d
saturncloudapp1 <149740534+saturncloudapp1@users.noreply.github.com>
2023-11-11 21:05:53 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 # Instala as dependências do Node.js usando npm
+sudo apt update
+
+sudo apt upgrade -y
+
+sudo apt install npm -y
+
 npm install --prefix /home/jovyan/work
 
 node /home/jovyan/work/index.js
