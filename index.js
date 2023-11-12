cd5f52131e7c5828b739f38bc0984f0c946e859a
saturncloudapp1 <149740534+saturncloudapp1@users.noreply.github.com>
2023-11-12 12:39:22 -0300

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.get('/', async (req, res) => {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    res.setHeader('Access-Control-Allow-Credentials', true); // If needed
+    //res.setHeader('Access-Control-Allow-Credentials', true); // If needed
     //console.log("REQ RECEBIDA")
   await res.json({
       hello:'hello word'
