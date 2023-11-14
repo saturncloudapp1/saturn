72c1a4966e1d6e51f4a6f8b1743b745aef5ea21a
saturncloudapp1 <149740534+saturncloudapp1@users.noreply.github.com>
2023-11-14 15:42:15 -0300

    Update index2.js

--- index2.js
+++ index2.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 8000;
+const port = 8002;
 const cors = require('cors')
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
