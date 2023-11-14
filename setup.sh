a6a3e58efe3c1b9c1cd31e08b545a012bf330a24
saturncloudapp1 <149740534+saturncloudapp1@users.noreply.github.com>
2023-11-14 15:13:51 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,5 +7,15 @@ sudo apt upgrade -y
 sudo apt install npm -y
 
 npm install --prefix /home/jovyan/work
+#Nginx install
+sudo apt-get update
+
+# Instalar o Nginx
+sudo apt-get install -y nginx
+
+# Iniciar o serviço Nginx
+sudo service nginx start
+
+sudo service nginx status
 
 node /home/jovyan/work/index.js
