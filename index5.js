8af2af8d404e387b7b22271134695c230718e12f
saturncloudapp1 <149740534+saturncloudapp1@users.noreply.github.com>
2023-11-27 05:53:58 -0300

    Add files via upload

---
+++ index5.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const app = express();
+const port = 8005;
+const cors = require('cors')
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+const ipExterno = '0.0.0.0';
+
+app.use(
+  '/test', // O prefixo da URL que você deseja redirecionar
+  createProxyMiddleware({
+    target: 'https://localhost:3000',
+    changeOrigin: true,
+  })
+);
+app.use(cors({
+  origin: '*',  // Permitindo todas as origens (não seguro para produção)
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+}));
+app.get('/', async (req, res) => {
+      // Website you wish to allow to connect
+   /*res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');*/
+    //res.setHeader('Access-Control-Allow-Credentials', true); // If needed
+    //console.log("REQ RECEBIDA")
+  await res.json({
+      hello:'hello word 2 nginx'
+  })
+});
+
+app.get('/test', async (req, res) => {
+    //console.log("REQ RECEBIDA")
+    await res.json({
+      hello:'hello word teste'
+  })
+});
+
+app.listen(port,ipExterno, () => {
+  console.log(`Servidor Express está ouvindo na porta ${port}`);
+});
